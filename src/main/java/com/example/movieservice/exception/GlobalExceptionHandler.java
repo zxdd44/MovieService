ee9ce9745ae9385f9a8478f2c6e35ee5284f005a
zxdd44 <zxdd16354@gmail.com>
2026-04-01 15:49:18 +0300

    L

--- src/main/java/com/example/movieservice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/movieservice/exception/GlobalExceptionHandler.java
@@ -7,10 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import java.util.List;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
     private static final Logger LOG = LoggerFactory.getLogger(GlobalExceptionHandler.class);
