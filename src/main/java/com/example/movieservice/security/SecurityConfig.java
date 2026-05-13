61065f646a1e285a7b476fc5e3ada6b564db06f1
zxdd44 <zxdd16354@gmail.com>
2026-05-13 14:34:38 +0300

    att11

--- src/main/java/com/example/movieservice/security/SecurityConfig.java
+++ src/main/java/com/example/movieservice/security/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
             }))
 
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/actuator/health").permitAll()
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
