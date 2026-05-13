90cb075039be066f63d1dca825d307da0e1e712f
zxdd44 <zxdd16354@gmail.com>
2026-05-13 10:52:28 +0300

    att5

--- src/main/java/com/example/movieservice/security/SecurityConfig.java
+++ src/main/java/com/example/movieservice/security/SecurityConfig.java
@@ -62,9 +62,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173",
-            "https://movie-service-beryl.vercel.app",
-            "https://movie-service-56ffm5lf3-gfhgfhf-s-projects.vercel.app",
-            "movieservice-production-dd97.up.railway.app"));
+            "https://*.vercel.app",
+            "https://movieservice-production-dd97.up.railway.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
