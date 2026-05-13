7d7665dc1ff1800910b55a2d6f2013b92ebfc636
zxdd44 <zxdd16354@gmail.com>
2026-05-13 08:45:41 +0300

    Oleg deployer

--- frontend-app/src/api.jsx
+++ frontend-app/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://movieservice-production-dd97.up.railway.app/api',
     headers: {
         'Content-Type': 'application/json'
     },
